80f3a1a573f7440c9b6ac19817d255289c2aef33
ArtemCaster <97943938+ArtemCaster@users.noreply.github.com>
2022-04-04 09:39:33 +0300

    start project

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+
+  wow = new WOW(
+    {
+      boxClass: 'wow',
+      animateClass: 'animate__animated --active',
+      offset: 300,
+      mobile: false,
+    }
+  )
+
+  wow.init();
+
+
+});
